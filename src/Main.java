88f64581548882a52e0ca1210cc1941d46927b5f
Balasagyn <112805169+Balasagyn@users.noreply.github.com>
2022-10-20 17:20:31 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Car.counter);
+        Car c1 = new Car(2013, "AUDI");
+        Car c2 = new Car(2011, "Toyota");
+        System.out.println(Car.counter);
+        c1.drive();
+        Car.addition(8, 9);
     }
 }
